005b93209eb90732c19a29e0d0dc8f7d2ed6400c
Admir Trakic <atrakic@users.noreply.github.com>
2023-01-12 21:33:59 +0100

    Update params.json

--- makesite.py
+++ makesite.py
@@ -175,7 +175,7 @@ def main():
     # Default parameters.
     params = {
         'base_path': '',
-        'subtitle': 'Foo Bar',
+        'subtitle': 'Lorem Ipsum',
         'author': 'pwNd',
         'site_url': 'http://localhost:8000',
         'current_year': datetime.datetime.now().year
